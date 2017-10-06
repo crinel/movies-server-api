f1c4ec8f6c6269aba1605dc55bba72ec6b5652aa
Melania Moldovan <mmoldovan@stylyze.com>
2017-10-06 18:45:51 +0300

    Updated get movie and get movies, and the data in the db

--- app/routes/movie_routes.js
+++ app/routes/movie_routes.js
@@ -117,39 +117,99 @@ module.exports = function(app, db) {
 			if (err) {
 				res.send({'error':'An error has occurred'});
 			} else {
-				res.send(items);
+				const itemsWithoutDetails = items.map(item => {
+					return {
+						_id: item._id,
+						Title: item.Title,
+						Year: item.Year,
+						Runtime: item.Runtime,
+						Genre: item.Genre,
+						Language: item.Language,
+						Country: item.Country,
+						Poster: item.Poster,
+						imdbRating: item.imdbRating,
+						imdbVotes: item.imdbVotes,
+						imdbID: item.imdbID,
+						Type: item.Type
+					};
+				});
+
+				res.send(itemsWithoutDetails);
 			}
 		});
 	});
+
+  app.delete('/private/movies/', (req, res) => {
+    const authToken = req.get('x-auth-token');
+    //PROTECTED ROUTE check if user is logged in
+    db.collection('session').findOne({'_id': authToken}, (err, item) => {
+      if (err) {
+        res.status(500);
+      } else {
+        console.log(item);
+        if (item) {
+          db.collection('movies').remove({}, (err, item) => {
+            if (err) {
+              res.send({'error':'An error has occurred'});
+            } else {
+              res.send('Movies deleted!');
+            }
+          });
+        } else {
+          res.status(403);
+          res.json({
+            message: "You need to be authenticated to be able to delete a movie",
+          });
+        }
+      }
+    });
+  });
 
 	// Populate Movies Collection
 	app.post('/movies/all', (req, res) => {
 		let promises = [];
 
 		for (let page = 1; page <= omdbapi.noOfMovies / 10; page++) {
-			promises.push(populateMovies(db, page));
+			promises.push(getAllMoviesDetails(db, page));
 		}
 
-		Promise.all(promises).then(values => {
-			res.send(values);
+		Promise.all(promises).then(arraysOfMovies => {
+			const movies = arraysOfMovies
+											.reduce((acc, arrayOfMovies) => {
+												console.log('array of movies length', arrayOfMovies.length);
+												return acc.concat(arrayOfMovies);
+											}, [])
+											.map((movieString) => JSON.parse(movieString));
+      db.collection('movies').insertMany(movies)
+        .then((result) => {
+          console.log('success',result.ops.length);
+          res.send(result.ops);
+        })
+        .catch((err) => {
+          console.log('error');
+          res.send({ 'error': 'An error has occurred when populating movies' });
+        });
 		})
 	});
-
 };
 
-const populateMovies = (db, page) => {
-	return rp(`${omdbapi.baseurl}&s=a&page=${page}`)
-		.then((response) => {
-			var movies = JSON.parse(response).Search;
-			return db.collection('movies').insertMany(movies)
-				.then((result) => {
-					return Promise.resolve(result.ops);
-				})
-				.catch((err) => {
-					return Promise.reject({ 'error': 'An error has occurred' });
-				});
-		})
-		.catch((err) => {
-			return Promise.reject({'error':'There was an error with the request from OMDB API'});
-		});
-}
+const getAllMoviesDetails = (db, page) => {
+  return rp(`${omdbapi.baseurl}&s=a&page=${page}`)
+    .then((response) => {
+      const movies = JSON.parse(response).Search;
+
+      let detailsPromises = movies.map((movie) => {
+        return rp(`${omdbapi.baseurl}&i=${movie.imdbID}`);
+      });
+
+      console.log(detailsPromises.length);
+
+			return Promise.all(detailsPromises)
+        .catch((err) => {
+          return Promise.reject({'error':'There was an error with the details request from OMDB API'});
+        });
+    })
+    .catch((err) => {
+        return Promise.reject({'error':'There was an error with the request from OMDB API'});
+    });
+};
